a2615f5c886357478128aedaa65d09c93a52dd86
saktis <sakti.sanjaya@gmail.com>
2015-11-10 16:43:35 +0800

    Add module

---
+++ raw-body-parser.js
@@ -0,0 +1,21 @@
+/**
+ * Simple module to retrieve raw body part of a request.
+ */
+exports = module.exports = function() {
+	return function(req, res, next) {
+		req.rawBody = new Buffer(0);
+	
+		req.on('data', function(chunk) {
+			req.rawBody = Buffer.concat([req.rawBody, chunk]);
+		});
+	
+		req.on('end', function() {
+			next();
+		});
+	};
+};
+
+/**
+ * Initial version.
+ */
+exports.version = '0.0.1';
